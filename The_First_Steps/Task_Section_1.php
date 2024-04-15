d7c27673025bae629f4bd8c7b03456efd1d68401
sabski03 <saba27ge@gmail.com>
2024-04-15 21:41:11 +0400

    Section1

---
+++ The_First_Steps/Task_Section_1.php
@@ -0,0 +1,18 @@
+<?php
+
+
+/*
+   Step 1:  Use the Echo Function to say hello with html h1 tags embedded inside php.
+
+
+   Step 2: Write a comment above the echo function and explain
+   what that function is doing.
+
+
+   */
+
+//this function will say hello with html h1 tags embedded inside php!
+echo "<h1>Hello this is Section 1 <br> This is the First Task and its Time to move to the next section which is <br><br> ---> Data Types & More <---</h1>";
+
+
+?>
