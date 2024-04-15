2714b39e027aa2e7e058034e6ddf08a592ec1ca4
sabski03 <saba27ge@gmail.com>
2024-04-15 21:26:48 +0400

    Section 1

---
+++ The_First_Steps/comments.php
@@ -0,0 +1,24 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Documents</title>
+</head>
+<body>
+
+<?php
+
+    //this is a single line comment
+
+    /*
+    this is a
+    multiline
+    comment
+    */
+
+
+    //this function bellow displays text
+    echo "hello next developer";
+?>
+
+</body>
+</html>
