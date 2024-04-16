f2c8beb44db552fec1cac34265175ca835d0934b
sabski03 <saba27ge@gmail.com>
2024-04-16 17:26:33 +0400

    Section 5

---
+++ Section5-Built-in_Functions/string_functions.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Documents</title>
+</head>
+<body>
+
+<?php
+
+$string = "Hello Saba";
+
+echo strlen($string) . '<br>';
+
+echo strtoupper($string) . '<br>';
+
+echo strtolower($string) . '<br>';
+
+echo strrev($string) . '<br>';
+
+?>
+
+</body>
+</html>
