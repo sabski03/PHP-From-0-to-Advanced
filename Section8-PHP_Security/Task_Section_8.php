ed699cf68f8e27a4d8703822683d3316dab441ef
sabski03 <saba27ge@gmail.com>
2024-04-19 15:22:03 +0400

    Section 8

--- Section8-PHP_Security/Task_Section_8.php
+++ Section8-PHP_Security/Task_Section_8.php
@@ -4,7 +4,34 @@
     <title>Documents</title>
 </head>
 <body>
+<?php
 
+/*
+    Step 1 - Make a variable with some text as value
+    Step 2 - Use crypt() function to encrypt it
+    Step 3 - Assign the crypt result to a variable
+    Step 4 - echo the variable
+ */
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    echo "Before - " . $name . '<br>';
+
+    $salt = "iusesomecrazystring22";
+    $name = crypt($name, $salt);
+
+    echo "After - " . $name;
+
+}
+
+
+?>
+
+<form action="Task_Section_8.php" method="POST">
+    <label for="name">Name</label>
+    <input type="text" name="name">
+    <input name="submit" type="submit" value="Submit">
+</form>
 
 
 </body>
